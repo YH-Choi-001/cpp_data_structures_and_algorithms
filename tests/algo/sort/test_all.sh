7eda2b92c19548554c218eaa1f3fd47be7602ac5
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2025-10-22 20:49:36 +0100

    add test cases for sorts

--- tests/algo/sort/test_all.sh
+++
@@ -1,8 +0,0 @@
-for file in *.cpp; do
-    if [ -f "$file" ]; then
-        output_name="${file/.cpp/.out}"
-        g++ "$file" -o "$output_name"
-        "./${output_name}"
-        rm "$output_name"
-    fi
-done
