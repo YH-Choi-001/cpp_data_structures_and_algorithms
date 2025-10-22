213b88b2fd8b70dd99cf342e28b48cd751141e5e
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2025-10-22 17:39:18 +0100

    refactor test cases for sorts

--- tests/algo/sort/test_quick_sort.cpp
+++ tests/algo/sort/test_quick_sort.cpp
@@ -2,11 +2,9 @@
 
 #include <iostream>
 
-using yh::algo::sort::quick_sort;
-
 template<typename T>
 bool check(T *const elements, const T *const results, const size_t len) {
-    quick_sort(elements, 0, len);
+    yh::algo::sort::quick_sort(elements, 0, len);
     for (size_t i = 0; i < len; i++) {
         if (elements[i] != results[i]) {
             return false;
@@ -15,129 +13,16 @@ bool check(T *const elements, const T *const results, const size_t len) {
     return true;
 }
 
+#include "test_sort.h"
+
 int main() {
     std::cout << "Testing " << __FILE__ << std::endl;
-    unsigned int failed_testcase_count = 0;
-    size_t testcase_index = 0;
-
-    #define ELEMENTS(...) \
-        int elements [] = __VA_ARGS__;
-    
-    #define RESULTS(...) \
-        const int results [] = __VA_ARGS__;
-    
-    #define CHECK(l) \
-        const size_t len = l; \
-        const bool passed = check<int>(elements, results, len); \
-        std::cout << "Testcase #" << testcase_index; \
-        if (passed) { \
-            std::cout << " passed." << std::endl; \
-        } else { \
-            ++failed_testcase_count; \
-            std::cout << " failed: "; \
-            std::cout << "Expected: "; \
-            for (size_t i = 0; i < len; i++) { \
-                std::cout << results[i] << "\t"; \
-            } \
-            std::cout << "Returned: "; \
-            for (size_t i = 0; i < len; i++) { \
-                std::cout << elements[i] << "\t"; \
-            } \
-            std::cout << std::endl; \
-        } \
-        ++testcase_index; \
-
-    {
-        ELEMENTS({})
-        RESULTS ({})
-        CHECK   (0)
-    }
-    {
-        ELEMENTS({0})
-        RESULTS ({0})
-        CHECK   (1)
-    }
-    {
-        ELEMENTS({8})
-        RESULTS ({8})
-        CHECK   (1)
-    }
-    {
-        ELEMENTS({-5})
-        RESULTS ({-5})
-        CHECK   (1)
-    }
-    {
-        ELEMENTS({3, 8})
-        RESULTS ({3, 8})
-        CHECK   (2)
-    }
-    {
-        ELEMENTS({8, 3})
-        RESULTS ({3, 8})
-        CHECK   (2)
-    }
-    {
-        ELEMENTS({8, 8})
-        RESULTS ({8, 8})
-        CHECK   (2)
-    }
-    {
-        ELEMENTS({-6, 2})
-        RESULTS ({-6, 2})
-        CHECK   (2)
-    }
-    {
-        ELEMENTS({2, -6})
-        RESULTS ({-6, 2})
-        CHECK   (2)
-    }
-    {
-        ELEMENTS({-8, -3})
-        RESULTS ({-8, -3})
-        CHECK   (2)
-    }
-    {
-        ELEMENTS({-3, -8})
-        RESULTS ({-8, -3})
-        CHECK   (2)
-    }
-    {
-        ELEMENTS({5, 8, -1})
-        RESULTS ({-1, 5, 8})
-        CHECK   (3)
-    }
-    {
-        ELEMENTS({62, 15, 38, 41, 9, 28, 57})
-        RESULTS ({9, 15, 28, 38, 41, 57, 62})
-        CHECK   (7)
-        // 15 62 38 41 9 28 57
-        // 15 38 62 41 9 28 57
-        // 15 38 41 62 9 28 57
-        // 15 38 41 9 62 28 57
-        // 15 38 41 9 28 62 57
-        // 15 38 41 9 28 57 62
-
-        // 15 38 41 9 28 x x
-        // 15 9 41 38 28 x x
-        // 15 9 28 38 41 x x
-        
-        // 15 9 x x x x x
-        // 9 15 x x x x x
-        // done
-
-        // x x x 38 41 x x
-        // x x x 38 41 x x
-    }
-    {
-        ELEMENTS({62, 15, 38, 41, 9, 28, 57, 5})
-        RESULTS ({5, 9, 15, 28, 38, 41, 57, 62})
-        CHECK   (8)
-    }
+    const unsigned int failed_testcase_count = test();
     if (failed_testcase_count == 0) {
-        std::cout << "All passed: " << __FILE__ << std::endl;
+        std::cout << "All passed: ";
     } else {
-        std::cout << failed_testcase_count << " failed: " << __FILE__ << std::endl;
+        std::cout << failed_testcase_count << " failed: ";
     }
+    std::cout << __FILE__ << std::endl;
     return failed_testcase_count;
 }
---
+++ tests/algo/sort/test_sort.h
@@ -0,0 +1,120 @@
+#ifndef TEST_SORT_H
+#define TEST_SORT_H
+
+#include <cstddef>
+
+unsigned int test() {
+    unsigned int failed_testcase_count = 0;
+    size_t testcase_index = 0;
+
+    #define ELEMENTS(...) \
+        int elements [] = __VA_ARGS__;
+    
+    #define RESULTS(...) \
+        const int results [] = __VA_ARGS__;
+    
+    #define CHECK(l) \
+        const size_t len = l; \
+        const bool passed = check<int>(elements, results, len); \
+        std::cout << "Testcase #" << testcase_index; \
+        if (passed) { \
+            std::cout << " passed." << std::endl; \
+        } else { \
+            ++failed_testcase_count; \
+            std::cout << " failed: "; \
+            std::cout << "Expected: "; \
+            for (size_t i = 0; i < len; i++) { \
+                std::cout << results[i] << "\t"; \
+            } \
+            std::cout << "Returned: "; \
+            for (size_t i = 0; i < len; i++) { \
+                std::cout << elements[i] << "\t"; \
+            } \
+            std::cout << std::endl; \
+        } \
+        ++testcase_index; \
+
+    {
+        ELEMENTS({})
+        RESULTS ({})
+        CHECK   (0)
+    }
+    {
+        ELEMENTS({0})
+        RESULTS ({0})
+        CHECK   (1)
+    }
+    {
+        ELEMENTS({8})
+        RESULTS ({8})
+        CHECK   (1)
+    }
+    {
+        ELEMENTS({-5})
+        RESULTS ({-5})
+        CHECK   (1)
+    }
+    {
+        ELEMENTS({3, 8})
+        RESULTS ({3, 8})
+        CHECK   (2)
+    }
+    {
+        ELEMENTS({8, 3})
+        RESULTS ({3, 8})
+        CHECK   (2)
+    }
+    {
+        ELEMENTS({8, 8})
+        RESULTS ({8, 8})
+        CHECK   (2)
+    }
+    {
+        ELEMENTS({-6, 2})
+        RESULTS ({-6, 2})
+        CHECK   (2)
+    }
+    {
+        ELEMENTS({2, -6})
+        RESULTS ({-6, 2})
+        CHECK   (2)
+    }
+    {
+        ELEMENTS({-8, -3})
+        RESULTS ({-8, -3})
+        CHECK   (2)
+    }
+    {
+        ELEMENTS({-3, -8})
+        RESULTS ({-8, -3})
+        CHECK   (2)
+    }
+    {
+        ELEMENTS({5, 8, -1})
+        RESULTS ({-1, 5, 8})
+        CHECK   (3)
+    }
+    {
+        ELEMENTS({62, 15, 38, 41, 9, 28, 57})
+        RESULTS ({9, 15, 28, 38, 41, 57, 62})
+        CHECK   (7)
+    }
+    {
+        ELEMENTS({-15, 30, -60, 76, -93, -5, 83, 22, -98, -64, -51, -81, -8, 97, 34, 69, 38})
+        RESULTS ({-98, -93, -81, -64, -60, -51, -15, -8, -5, 22, 30, 34, 38, 69, 76, 83, 97})
+        CHECK   (17)
+    }
+    {
+        ELEMENTS({-15, 30, -60, 76, 0, -93, -5, 83, 22, -98, -64, -51, -81, -8, 97, 34, 69, 38})
+        RESULTS ({-98, -93, -81, -64, -60, -51, -15, -8, -5, 0, 22, 30, 34, 38, 69, 76, 83, 97})
+        CHECK   (18)
+    }
+    {
+        ELEMENTS({-15, 30, -60, 76, 0, -93, -5, 83, 22, -98, -64, -51, -81, -8, 97, 34, -89, 69, 38})
+        RESULTS ({-98, -93, -89, -81, -64, -60, -51, -15, -8, -5, 0, 22, 30, 34, 38, 69, 76, 83, 97})
+        CHECK   (19)
+    }
+    return failed_testcase_count;
+}
+
+#endif
