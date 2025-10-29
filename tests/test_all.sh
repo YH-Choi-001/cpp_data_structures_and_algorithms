b1c304eccdecd4f7527fdf8ddbdbe011e2014faa
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2025-10-29 11:26:38 +0000

    grep only failed lines or all-pass from tests

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,2 +1,2 @@
 # compile every test*.cpp file individually into test*.cpp.out file
-find ./ -name "test*.cpp" -exec g++ {} -o {}.out \; -exec {}.out \; -exec rm {}.out \;
+find ./ -name "test*.cpp" -exec g++ {} -o {}.out \; -exec {}.out \; -exec rm {}.out \; | grep -E "(All passed|failed)"
