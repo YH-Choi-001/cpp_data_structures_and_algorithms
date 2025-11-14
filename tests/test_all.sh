dc6a80c755289f73d0dfabf94b034bfb35a8eb95
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2025-11-14 21:49:15 +0000

    switch testing to makefiles

--- tests/test_all.sh
+++
@@ -1,2 +0,0 @@
-# compile every test*.cpp file individually into test*.cpp.out file
-find ./ -name "test*.cpp" -exec g++ {} -o {}.out 2> /dev/null \; -exec {}.out \; -exec rm {}.out \; | grep -E "(All passed|failed)"
